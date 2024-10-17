0c789082a2ace4075c07d973a9a9625f2622d330
Matin <matinjm7247@gmail.com>
2024-10-17 04:23:35 +0330

    feat: init first grpc app with some errors <<< fix it as soon as possible

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserServiceController, CreateUserRequest,UpdateUserRequest, UserServiceControllerMethods, GetUserRequest, PaginationRequest } from '@app/common';
+import { Observable } from 'rxjs';
+
+@Controller()
+@UserServiceControllerMethods()
+export class UsersController implements UserServiceController {
+  constructor(private readonly usersService: UsersService) {}
+
+  createUser(createUserDto: CreateUserRequest) {
+    return this.usersService.create(createUserDto);
+  }
+
+  getUsers() {
+    return this.usersService.findAll();
+  }
+
+  getUser(getUserDto:GetUserRequest) {
+    return this.usersService.findOne(getUserDto.id);
+  }
+
+  updateUser(updateUserDto: UpdateUserRequest) {
+    return this.usersService.update(updateUserDto.id, updateUserDto);
+  }
+
+  removeUser(getUserDto:GetUserRequest) {
+    return this.usersService.remove(getUserDto.id);
+  }
+
+  queryUsers(paginationDtoStream: Observable<PaginationRequest>) {
+      return this.usersService.queryUsers(paginationDtoStream)
+  }
+}
